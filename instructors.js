93da48c0ce8aba908fbfe2f9912a92110e741948
vktorbr <vktorbr@gmail.com>
2020-09-04 19:19:12 -0300

    ajustando o atributo data do form

--- instructors.js
+++ instructors.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-
+const data = require('./data.json');
 //create
 exports.post = function(req, res){
     const keys = Object.keys(req.body);
@@ -8,12 +8,17 @@ exports.post = function(req, res){
         if(req.body[key] == '')
             return res.send('Please, fill all fields!');
     }
-    fs.writeFile('data.json', JSON.stringify(req.body), function(err){
+    req.body.birth = Date.parse(req.body.birth);
+    req.body.created_at = Date.now();
+
+    data.instructors.push(req.body);
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
         if(err) return res.send('Write file error!');
 
         return res.redirect('/instructors');
     })
-    return res.send(req.body);
+    //return res.send(req.body);
     
 }
 //update
