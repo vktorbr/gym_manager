e8a80a356571152e5fa4470666468cfdd57358cb
vktorbr <vktorbr@gmail.com>
2020-09-07 19:09:04 -0300

    pagina de edicao do instrutor

--- instructors.js
+++ instructors.js
@@ -1,5 +1,26 @@
 const fs = require('fs');
 const data = require('./data.json');
+const { age } = require('./utils');
+
+//show
+exports.show = function(req, res){
+    const {id} = req.params;
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id;
+    })
+
+    if(!foundInstructor) return res.send('Instructor not found!');
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(','),
+        created_at: new Intl.DateTimeFormat('pt-BR').format(foundInstructor.created_at)
+    }
+
+    return res.render('instructors/show', {instructor});
+}
+
 //create
 exports.post = function(req, res){
     const keys = Object.keys(req.body);
---
+++ utils.js
@@ -0,0 +1,18 @@
+module.exports = {
+    age: function(timestamp){
+        const today = new Date();
+        const birthDate = new Date(timestamp);
+
+        let age = today.getFullYear() - birthDate.getFullYear();
+
+        const month = today.getMonth() - birthDate.getMonth();
+
+        const day = today.getDate() - birthDate.getDate();
+
+        if(month < 0 || month == 0 && day < 0){
+            age = age - 1;
+        }
+
+        return age;
+    }
+}
