59516621a9b705c2d274f838dd1812e8d461108c
vktorbr <vktorbr@gmail.com>
2020-09-08 16:50:45 -0300

    preenchendo dados do formulario da pagina de edicao

--- instructors.js
+++ instructors.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const data = require('./data.json');
-const { age } = require('./utils');
+const { age, date } = require('./utils');
 
 //show
 exports.show = function(req, res){
@@ -54,6 +54,21 @@ exports.post = function(req, res){
     //return res.send(req.body);
     
 }
-//update
 
+//edit
+exports.edit = function(req, res){
+    const {id} = req.params;
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id;
+    })
+
+    if(!foundInstructor) return res.send('Instructor not found!');
+
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+
+    return res.render('instructors/edit', {instructor});
+}
 //delete
