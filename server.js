09440cf55e818a073f69525133c787a129c09df9
vktorbr <vktorbr@gmail.com>
2020-09-20 18:45:47 -0300

    funcao de deletar instrutor

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const routes = require('./routes');
-
+const methodOverride = require('method-override');
 const server = express();
 
 //req.body
 server.use(express.urlencoded({extended: true}));
 server.use(express.static('public'));
+server.use(methodOverride('_method'));
 server.use(routes);
 
 server.set('view engine', 'njk');
