6a9ad6da5a5dff77b7460fa6ed3dcb8897655e54
vktorbr <vktorbr@gmail.com>
2020-09-21 16:33:14 -0300

    pagina para listar instrutores

--- instructors.js
+++ instructors.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const data = require('./data.json');
 const { age, date } = require('./utils');
 
+exports.index = function(req, res){
+    return res.render('instructors/index', {instructors: data.instructors});
+}
+
 //show
 exports.show = function(req, res){
     const {id} = req.params;
@@ -91,6 +95,7 @@ exports.put = function(req, res){
         ...foundInstructor,
         ...req.body,
         birth: Date.parse(req.body.birth),
+        id: Number(req.body.id),
     }
 
     data.instructors[index] = instructor;
